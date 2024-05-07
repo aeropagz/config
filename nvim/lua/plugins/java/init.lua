8940a3ecfd5d7974dd23cf9d5707e1a19e0ccf20
klaaspelzer <klaas.pelzer@gmail.com>
2024-05-07 12:44:40 +0200

    added new java deps

--- nvim/lua/plugins/java/init.lua
+++ nvim/lua/plugins/java/init.lua
@@ -2,6 +2,7 @@ return {
   "nvim-java/nvim-java",
   dependencies = {
     "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-refactor",
     "nvim-java/nvim-java-core",
     "nvim-java/nvim-java-test",
     "nvim-java/nvim-java-dap",
@@ -22,11 +23,11 @@ return {
       opts = {
         handlers = {
           ["jdtls"] = function()
-            vim.g.autoformat = false
             require("java").setup()
           end,
         },
       },
     },
   },
+  opts = {},
 }
