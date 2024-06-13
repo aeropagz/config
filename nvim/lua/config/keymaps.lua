8756c974c6dc2bea53ae235c6ef45da127844ad5
klaaspelzer <klaas.pelzer@gmail.com>
2024-06-13 12:13:26 +0200

    added new keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
-vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<F2>", function()
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
 
-vim.keymap.set('n', '<F6>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F7>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F8>', function() require('dap').step_out() end)
+vim.keymap.set("n", "<F6>", function()
+  require("dap").step_over()
+end)
+vim.keymap.set("n", "<F7>", function()
+  require("dap").step_into()
+end)
+vim.keymap.set("n", "<F8>", function()
+  require("dap").step_out()
+end)
+vim.keymap.set("n", "<", "{")
+vim.keymap.set("n", ">", "}")
